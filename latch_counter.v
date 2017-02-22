b25fb9865f1501b5db605483be0b79d2b867df95
sabertazimi <sabertazimi@gmail.com>
2017-02-22 10:48:59 +0800

    add(verilog-architecture): create components for mips cpu
    issue sabertazimi/awesome-notes#99

---
+++ latch_counter.v
@@ -0,0 +1,35 @@
+/**
+ * @module latch_counter
+ * @brief latch counter (latching when reaching max vlaue)
+ * @input clk clock signal
+ * @input rst reset signal
+ * @output en enable signal 
+ * @output count counting value
+ */
+module latch_counter
+(
+    input clk,
+    input rst,
+    input en,
+    output reg count
+);
+
+    initial begin
+        count <= 0;
+    end
+
+    always @(posedge clk) begin
+        if (rst) begin
+            count <= 0 ;
+        end else if (en) begin
+            if (count != 1) begin
+                count <= count + 1;
+            end else begin
+                count <= count;
+            end
+        end else begin
+            count <= count;
+        end
+    end
+
+endmodule // latch_counter
